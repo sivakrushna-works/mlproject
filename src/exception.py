e0cbbc92a1e5206ad094a5e72944b56ad8242cb0
sivakrushna-works <sivakrushna.works@gmail.com>
2024-12-07 13:26:11 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_Detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -14,14 +14,20 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_Detail(error_message,error_detail=error_detail)
+        self.log_error()
         
 
     def __str__(self):
         return self.error_message
     
-
-
+    def log_error(self):
+        # Log the error message and details automatically
+        logging.error(self.error_message)
     
-
-
+if __name__=="__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("my log started")
+        raise CustomException(e, sys)
 
